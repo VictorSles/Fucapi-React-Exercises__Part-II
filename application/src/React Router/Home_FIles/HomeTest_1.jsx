59a6c09d874b4b7fc41af343aabf7fa9ae099973
VictorSles <israelvictords@gmail.com>
2024-11-19 18:38:39 -0400

    ,

--- application/src/React Router/Home_FIles/HomeTest_1.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const HomeTest_1 = () => {
-  return (
-    <div>HomeTest_1</div>
-  )
-}
-
-export default HomeTest_1
--- application/src/main.jsx
+++ application/src/main.jsx
@@ -2,31 +2,37 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-{/* Import das páginas criadas */}
+{/* Import das páginas criadas */ }
 import Home from './React Router/Home.jsx'
 import Contact from './React Router/Contact.jsx'
 import Products from './React Router/Products.jsx'
 import Nos from './React Router/Nos.jsx'
-{/* Import de Configuração do React Router */}
+{/* Import de Configuração do React Router */ }
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-{/* Criação de Array para import e montagem de paginas/rotas, no qual irá armazenar as variáveis criadas a partir do import das rotas como objetos */}
-const router_array = createBrowserRouter ([
-{
-  path:"/",
-  element: <Home/>
-},
-{
-  path: "contatos",
-  element: <Contact/>
-},
-{
-  path: "produtos",
-  element: <Products/>
-},
-{
-  path: "sobre-nos",
-  element: <Nos/>
-}
+{/* Criação de Array para import e montagem de paginas/rotas, no qual irá armazenar as variáveis criadas a partir do import das rotas como objetos */ }
+const router_array = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: 'contato',
+        element: <Contact/>
+      },
+      {
+        path: 'produtos',
+        element: <Products/>
+      },
+      {
+        path: 'sobre-nos',
+        element: <Nos/>
+      }
+    ]
+  }
 ])
 
 createRoot(document.getElementById('root')).render(
